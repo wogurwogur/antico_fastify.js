b312cd378029207a93b330c6560ba85d74dbe88f
재혁 최 <wogur64@gmail.com>
2026-01-07 17:34:51 +0900

    chore: data select add

--- src/repositories/notice.repository.js
+++ src/repositories/notice.repository.js
@@ -1,6 +1,11 @@
 import db from "../db/mysql.js"
 
 export async function getNoticeAll() {
-  const [rows] = await db.query("select 1 as iii");
+  const [rows] = await db.query("select * from notice");
   return rows;
+}
+
+export async function noticeInsert(noticeVO){
+    const [noticeResult] = await query("INSERT INTO(TITLE, CONTENT, VIEW_COUNT, IS_PINNED, IS_DELETED, CREATED_AT, UPDATED_AT)"
+        + " VALUES(?, ?, 0, )")
 }
